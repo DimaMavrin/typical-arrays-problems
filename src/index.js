8bddcc0de6faf81d9d772296e7f098709750ff5b
Dima Mavrin <ieclipsee@gmail.com>
2021-03-08 21:28:32 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-
 exports.min = function min (array) {
-  return 0;
+    return (array && array.length) ? Math.min(...array) : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+    return (array && array.length) ? Math.max(...array) : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
-}
+
+    if (array && array.length) {
+        return array.reduce(function (accumulator, currentValue){
+        return (accumulator + currentValue) / array.length
+    })
+    } else {
+        return 0;
+    };
+
+};
